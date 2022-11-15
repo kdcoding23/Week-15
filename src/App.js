6d5871d4dcda6ad849f9c54bc05f85d5a5a42755
KO <ko@KOs-MacBook-Pro.home>
2022-11-14 20:03:09 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import House from "./components/house";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const HOUSES_ENDPOINT = "https://ancient-taiga-31359.herokuapp.com/api/houses";
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    // methods
+    this.addNewRoom = this.addNewRoom.bind(this);
+    this.deleteRoom = this.deleteRoom.bind(this);
+  }
+
+  render() {
+    // if the state isn't null, then go through all houses and list them out as House elements
+    const houses = this.state
+      ? this.state.houses.map((house, index) => 
+      <House
+        key={index}
+        data={house}
+        addNewRoom = {this.addNewRoom}
+        deleteRoom = {this.deleteRoom} />)
+      : null;
+      
+      return (
+        <div>
+          {houses}
+        </div>
+      );
+  }
+
+  // make http requests and get data for houses in state
+  componentDidMount() {
+    fetch(HOUSES_ENDPOINT)
+      .then(res => res.json())
+      .then(data => {
+      this.setState({
+        houses: data
+      });
+    });
+  }
+
+  // to find index of room you want to delete and then deleting just that one (using splice)
+  deleteRoom(e, house, room) {
+    const index = house.rooms.indexOf(room);
+    house.rooms.splice(index, 1);
+    // iterate through each house in the previous state, find the corresponding id of house we're updating, then update the list of houses on page in a new state
+    updateHouse(house)
+      .then(() => {
+        this.setState(state => {
+          for (let h of state.houses) {
+            if (h._id === house._id) {
+              let h = house;
+              break;
+            }
+          }
+          return state;
+        });
+      });
+      e.preventDefault();
+  }
+
+  // add new room and to specific house and update to page
+  addNewRoom(e, house, room) {
+    // const index = house.rooms.indexOf(room);
+    // house.rooms.splice(index, 1);
+    house.rooms.push(room)
+    updateHouse(house)
+      .then(() => {
+        this.setState(state => {
+          for (let h of state.houses) {
+            if (h._id === house._id) {
+              let h = house;
+              break;
+            }
+          }
+          return state;
+        });
+      });
+      e.preventDefault();
+  }
 }
 
-export default App;
+
+// send http request to make this update in API
+function updateHouse(house) {
+  return fetch(`${HOUSES_ENDPOINT}/${house._id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(house)
+  })
+}
---
+++ src/components/new-room-form.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default class NewRoomForm extends React.Component {
+    constructor(props) {
+        super(props);
+        // values that are in the NewRoomForm
+        this.state = {
+            nameValue: "",
+            areaValue: ""
+        }
+
+        // methods
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleAreaChange = this.handleAreaChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleNameChange(e) {
+        this.setState({nameValue: e.target.value});
+    }
+
+    handleAreaChange(e) {
+        this.setState({areaValue: e.target.value});
+    }
+
+    // changes local state when adding new room
+    handleClick(e) {
+        this.props.addNewRoom(e, this.props.data,
+            {name: this.state.nameValue, area: this.state.areaValue});
+        this.setState({nameValue: '', areaValue: ''});
+    }
+
+    render() {
+        return (
+            // updates the specific inputted value with corresponding method above
+            <div>
+                <input type="text" placeholder="Name" onChange={this.handleNameChange} value={this.state.nameValue} />
+                <input type="text" placeholder="Area" onChange={this.handleAreaChange} value={this.state.areaValue} />
+                <button onClick={this.handleClick}>Add Room</button>
+            </div>
+        );
+    }
+
+}
